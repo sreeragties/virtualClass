f918f4f62c1927f8e2c2bdca27bd0156486790bc
amith-jiji <amithjmadathil@gmail.com>
2020-07-06 07:30:12 +0530

    Registration Form Extended

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
 
 
 # Create your views here.
@@ -15,10 +16,10 @@ def dashboardView(request):
 
 def registerView(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login_url')
     else:
-        form = UserCreationForm()
+        form=SignUpForm()
     return render(request, 'registration/register.html', {'form': form})
